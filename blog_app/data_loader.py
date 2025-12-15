23a61a3c55f02ce896204e47477e63839556f1da
tanchik <lytkinatatiana2008@gmail.com>
2025-12-15 18:53:21 +0300

    Revert "финальный проект"

--- blog_app/data_loader.py
+++ blog_app/data_loader.py
@@ -1,15 +1,13 @@
 import json
 from typing import List
-
-from .crud import posts, users
-from .models import Post, User
+from .models import User, Post
+from .crud import users, posts
 
 DATA_FILE = "data.json"
 
-
 def load_data():
     try:
-        with open(DATA_FILE, "r", encoding="utf-8") as f:
+        with open(DATA_FILE, 'r', encoding='utf-8') as f:
             data = json.load(f)
             for u in data.get("users", []):
                 users.append(User(**u))
@@ -21,14 +19,13 @@ def load_data():
     except Exception as e:
         print(f"Ошибка при загрузке данных: {e}")
 
-
 def save_data():
     data = {
         "users": [user.dict() for user in users],
-        "posts": [post.dict() for post in posts],
+        "posts": [post.dict() for post in posts]
     }
     try:
-        with open(DATA_FILE, "w", encoding="utf-8") as f:
+        with open(DATA_FILE, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=2, default=str)
         print("Данные успешно сохранены в файл.")
     except Exception as e:
